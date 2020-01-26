ef3f1d1972104bddbb10ca2514629dc34dd20e05
Harley McDowell <hmcdowell95@gmail.com>
2020-01-26 01:30:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,10 +4,11 @@ def countdown(number)
   while number > 0 
   puts "#{number} SECOND(S)!"
   number -= 1 
+  sleep(1)
   end 
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(num_secs)
-  sleep(12)
+  sleep(10)
 end
